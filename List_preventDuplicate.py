bc257eac8028102be38c91dc090fa046da9f439b
Candra Widjaja <39996056+fransiscuscandra@users.noreply.github.com>
2020-06-30 13:28:21 -0400

    Create List_preventDuplicate.py

---
+++ List_preventDuplicate.py
@@ -0,0 +1,6 @@
+
+backpack = ["sword","rubber duck","pizza","parachute","sword","sword"]
+backpack2 = {"sword","rubber duck","pizza","parachute","sword","sword"}
+
+print(backpack2)
+print("sword" in backpack2)  #to check if something in set 
