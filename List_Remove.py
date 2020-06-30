6b943617483cbd364879152177b73d60cfed65ad
Candra Widjaja <39996056+fransiscuscandra@users.noreply.github.com>
2020-06-30 13:24:30 -0400

    Create List_Remove.py

---
+++ List_Remove.py
@@ -0,0 +1,6 @@
+healthy = ["kale chips", "brocoli"]
+backpack = ["pizza", "frozen custard", "apple crisp", "kale chips"]
+
+if "kale chips" in healthy:
+    backpack.remove("kale chips")
+print(backpack)
